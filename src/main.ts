c1babb68ddb95040360d0688a2195254aeb6e414
HiiragiNemu <128921071+HiiragiNemu@users.noreply.github.com>
2026-08-03 23:06:12 +0800

    feat(i18n): initialize bilingual localization before viewer setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import './style.css'
 import './polyfills'
 import { setupViewer } from './viewer'
-import { installZhCnUi } from './viewer/localization/zhCN'
+import { installLocalization } from './viewer/localization/zhCN'
 import { installOfficialStageTextureResolver } from './viewer/stageTextureResolver'
 import { setupStageSelector } from './viewer/stages'
 
 installOfficialStageTextureResolver()
-installZhCnUi()
+installLocalization()
 setupViewer()
 void setupStageSelector()
